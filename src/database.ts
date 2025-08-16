32281efddc5de5762bba60a0ad02646c42ad4b1a
Victor Ruinivan <victorruinivanvg@gmail.com>
2025-08-16 19:07:51 -0300

    .

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,11 @@
 import knex from 'knex';
 
-export const setupKnex = knex({
+export const config = {
   client: 'sqlite',
   connection: {
     filename: './tmp/app.db',
   },
   useNullAsDefault: true,
-});
+};
+
+export const setupKnex = knex(config);
