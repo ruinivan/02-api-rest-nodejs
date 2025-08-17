fd7116e46cbcf3424f2ab62240c514585a50e350
Victor Ruinivan <victorruinivanvg@gmail.com>
2025-08-17 02:16:14 -0300

    .

--- db/migrations/20250816222109_add-session-id-to-transactions.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Knex } from 'knex';
-export declare function up(knex: Knex): Promise<void>;
-export declare function down(knex: Knex): Promise<void>;
-//# sourceMappingURL=20250816222109_add-session-id-to-transactions.d.ts.map
