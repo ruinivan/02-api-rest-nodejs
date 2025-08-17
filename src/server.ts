79d7e57fc4533bce30c4bb97bd8aa1f6e0885dd2
Victor Ruinivan <victorruinivanvg@gmail.com>
2025-08-17 03:51:56 -0300

    arrumando a porta no log

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { env } from './env';
 app
   .listen({ port: env.PORT })
   .then(() => {
-    console.log('HTTP Server running on http://localhost:3333');
+    console.log(`HTTP Server running on http://localhost:${env.PORT}`);
   })
   .catch((err) => {
     console.error('Error starting server:', err);
