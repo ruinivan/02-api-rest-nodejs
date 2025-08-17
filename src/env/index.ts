cccd0f6afc7fa44b56cceefc5d6f2b320d243205
Victor Ruinivan <victorruinivanvg@gmail.com>
2025-08-17 03:43:22 -0300

    arrumando

--- src/env/index.ts
+++ src/env/index.ts
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['dsv', 'test', 'prd']).default('prd'),
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_CLIENT: z.enum(['sqlite3', 'pg']).default('pg'),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
